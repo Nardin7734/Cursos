7abb61928feda87b97f240e488685c45fbd6d312
Nardin7734 <ddnardin@gmail.com>
2021-10-31 01:09:23 -0300

    Ajuste secret keys

--- DjangoForms/setup/settings.py
+++ DjangoForms/setup/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '67c84**t8rmrr3pm@4%d9&r@6q_yj9zs228!xwrkb8sz=@@#90'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
